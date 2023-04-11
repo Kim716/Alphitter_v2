67563fa13c0314047b529562e1f4cab70dce7322
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-04-11 10:42:21 +0800

    feat: install prop-types add rules

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,4 +11,24 @@ function Button({
   return <button>{children}</button>;
 }
 
+Button.propTypes = {
+  // 客製化對 props 的規則，primary,secondary,success,warning,danger 只能有一個 true
+  // checkVariationValue 為函式，第一個參數是 props，這裡直接解構出那五個
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    // Number(true) 等於 1, !!undefined 為 false，Number(false) 等於 0
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only ONE of primary, secondary, success, warning, danger can be TRUE!"
+      );
+    }
+  },
+};
+
 export default Button;
